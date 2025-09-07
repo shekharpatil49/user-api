6d89ec47acad880bcc78004b3b9f9b578ad0656f
shekharpatil49 <shekharpatil49@live.com>
2025-09-07 23:46:18 +0530

    Added get all user api

--- routes/user.js
+++ routes/user.js
@@ -43,4 +43,13 @@ router.put('/user/:id', async(req, res) =>{
     }
 });
 
+router.get('/users', async (req, res) =>{
+    try{
+        const user = await User.find();
+         res.status(200).json(user);
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
+});
+
 module.exports = router;
